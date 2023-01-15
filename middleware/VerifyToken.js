0e920b338c724a5bf31e48c8a2853b84842c72f8
Raihan Fathurrahman <cahtlogosari@gmail.com>
2023-01-15 22:34:47 +0700

    login register

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -1,14 +1,16 @@
 import  jwt  from "jsonwebtoken";
 
 export const verifyToken = ( req, res, next) => {
-    const authHeader = req.header['authorization'];
-    const token = authHeader && authHeader.split('')[1];
-    if(token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-        if(err) return res.sendStatus(403);
-        req.email = decode.email;
-        next();
-    })
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.status(401).json({message: "Authorization failed!"});
+    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    //     console.log(err)
+    //     if(err) return res.sendStatus(403);
+    //     req.email = decode.email;
+    // })
+    next();
 }
 
 // import jwt from "jsonwebtoken";
